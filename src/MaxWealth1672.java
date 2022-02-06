a0d6ddab622265b192e8258464527dca9d65a2d7
Gautam <gautamsethigagan@gmail.com>
2022-02-07 00:49:41 +0530

    printing the maximum of array elements in a 2D array

---
+++ src/MaxWealth1672.java
@@ -0,0 +1,35 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 07-02-2022
+ *   Time: 00:28
+ *   File: MaxWealth1672.java
+ */
+
+public class MaxWealth1672 {
+    public static void main(String[] args) {
+        int [][] array = {{1,2,3},{3,2,1}};
+        int result = maximumWealth(array);
+        System.out.println(result);
+
+    }
+
+    static int maximumWealth(int [][] accounts){
+
+        int max = Integer.MIN_VALUE;
+        for (int person = 0; person < accounts.length; person++) {
+            int sum =0;
+            for (int account = 0; account < accounts[person].length; account++) {
+                sum = sum+ accounts[person][account];
+
+
+            }
+
+            if(sum > max){
+                max = sum;
+            }
+
+
+        }
+        return max;
+    }
+}
