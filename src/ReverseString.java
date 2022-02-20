61382db912a2f395681c800c29d50d0c3d21c52a
Gautam <gautamsethigagan@gmail.com>
2022-02-20 22:26:02 +0530

    reversing character array

---
+++ src/ReverseString.java
@@ -0,0 +1,33 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 20-02-2022
+ *   Time: 22:14
+ *   File: ReverseString.java
+ */
+
+import java.util.Arrays;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        char[] S = new char[]{'a', 'b', 'c'};
+        char [] res = reverseString(S);
+        System.out.println(Arrays.toString(S));
+
+
+    }
+
+    public static char[] reverseString(char[] s) {
+        int n = s.length;
+        int start = 0;
+        int end = n-1;
+        while(start<end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] =  temp;
+            start++;
+            end--;
+        }
+
+        return s;
+    }
+}
