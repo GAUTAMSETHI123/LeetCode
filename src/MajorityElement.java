00bf3c21731011903a4521cd3d3a74827f6bf317
Gautam <gautamsethigagan@gmail.com>
2022-02-23 02:31:18 +0530

    Majority element in an array

---
+++ src/MajorityElement.java
@@ -0,0 +1,37 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 23-02-2022
+ *   Time: 02:26
+ *   File: MajorityElement.java
+ */
+
+import java.util.Arrays;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,1,1,2,4};
+
+        int res = majorityElement(arr);
+        System.out.println(res);
+
+
+    }
+
+    public static int majorityElement(int[] nums) {
+        for(int i=0; i<nums.length; i++){
+            int count =0;
+            for(int j=0; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+
+                }
+
+            }
+            if(count > nums.length/2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+    }
+
