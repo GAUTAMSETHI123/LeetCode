948ac38f7f7c463ed5e44dda61f8242d424cd460
Gautam <gautamsethigagan@gmail.com>
2022-03-03 23:59:21 +0530

    first unique character in a string

--- src/FirstUniqueCharInString.java
+++ src/FirstUniqueCharInString.java
@@ -6,4 +6,24 @@
  */
 
 public class FirstUniqueCharInString {
+    public static void main(String[] args) {
+
+    }
+    public int firstUniqChar(String s) {
+
+        for(int i=0;i<s.length();i++){
+            int flag=0;
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(i)==s.charAt(j) && i!=j){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+    
 }
