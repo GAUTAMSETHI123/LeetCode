21ff4613063dad78cba5150a1aa6f51feecfe766
Gautam <gautamsethigagan@gmail.com>
2022-03-07 23:55:32 +0530

    LeetCode 31 (Medium)

---
+++ src/FirstAndLastOccurenceOfAnElement.java
@@ -0,0 +1,51 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 07-03-2022
+ *   Time: 23:53
+ *   File: FirstAndLastOccurenceOfAnElement.java
+ */
+import java.util.*;
+public class FirstAndLastOccurenceOfAnElement {
+
+
+
+
+        static void findFirstAndLastPosition(int arr[],int n,int target)
+        {
+            // We initialize first and last as -1 if the target is not present in the array.
+            int first = -1;
+            int last = -1;
+
+            for(int i=0;i<n;i++)
+            {
+                if(arr[i] == target)
+                {
+                    first = i;
+                    break;
+                }
+            }
+
+            for(int i=n-1;i>=0;i--)
+            {
+                if(arr[i] == target)
+                {
+                    last = i;
+                    break;
+                }
+            }
+
+            System.out.println("The First occurrence of "+target+" at index : "+first+" and the Last occurrence is at index : "+last);
+        }
+
+        public static void main(String args[])
+        {
+            int arr[] = new int[]{2,3,4,5,5,7,7,7,9,11};
+            int n = arr.length;
+
+            int target = 7;
+            findFirstAndLastPosition(arr,n,target);
+
+        }
+
+    }
+
