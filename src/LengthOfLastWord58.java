da559197dfc0fe1502e8eebb98ee571028789a2a
Gautam <gautamsethigagan@gmail.com>
2022-03-08 23:35:34 +0530

    length of last word String question 58 (easy)

---
+++ src/LengthOfLastWord58.java
@@ -0,0 +1,31 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 08-03-2022
+ *   Time: 23:34
+ *   File: LengthOfLastWord58.java
+ */
+
+public class LengthOfLastWord58 {
+    public int lengthOfLastWord(String s) {
+        if(s==null || s.length() == 0)
+            return 0;
+
+        int result = 0;
+        int len = s.length();
+
+        boolean flag = false;
+        for(int i=len-1; i>=0; i--){
+            char c = s.charAt(i);
+            if((c>='a' && c<='z') || (c>='A' && c<='Z')){
+                flag = true;
+                result++;
+            }else{
+                if(flag)
+                    return result;
+            }
+        }
+
+        return result;
+
+    }
+}
