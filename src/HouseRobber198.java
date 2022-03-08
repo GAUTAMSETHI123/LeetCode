1c43b792a532257f658132dd02248937bd0f75c3
Gautam <gautamsethigagan@gmail.com>
2022-03-08 23:26:45 +0530

    House Robber question Medium question

---
+++ src/HouseRobber198.java
@@ -0,0 +1,30 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 08-03-2022
+ *   Time: 23:26
+ *   File: HouseRobber198.java
+ */
+
+public class HouseRobber198 {
+    public int rob(int[] nums) {
+
+        if(nums.length==1)
+            return nums[0];
+        if(nums.length==2)
+
+            return Math.max(nums[0],nums[1]);
+
+        int[] dp=new int[nums.length];
+
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+
+            int visited=nums[i]+dp[i-2];
+            int notvisit=dp[i-1];
+            dp[i]=Math.max(visited,notvisit);
+        }
+        return dp[nums.length-1];
+
+    }
+}
