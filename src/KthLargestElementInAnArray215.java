97e0357b34f3b9ab6ca4337bb6f051469399b7e9
Gautam <gautamsethigagan@gmail.com>
2022-03-12 23:43:23 +0530

    added function

--- src/KthLargestElementInAnArray215.java
+++ src/KthLargestElementInAnArray215.java
@@ -5,5 +5,26 @@
  *   File: KthLargestElementInAnArray215.java
  */
 
+import java.util.PriorityQueue;
+
 public class KthLargestElementInAnArray215 {
+    public int findKthLargest(int[] nums, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int i =0;
+        while(i<k){
+            pq.add(nums[i]);
+            i++;
+        }
+        while(i<nums.length){
+            int min = pq.element();
+            if(nums[i]>min){
+                pq.remove();
+                pq.add(nums[i]);
+            }
+            i++;
+        }
+        return pq.remove();
+
+    }
 }
