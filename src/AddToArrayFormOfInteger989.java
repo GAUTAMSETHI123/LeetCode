5117e2b89c051fc604e73293a5da2ef787d4fb31
Gautam <gautamsethigagan@gmail.com>
2022-03-17 22:36:22 +0530

    .

--- src/AddToArrayFormOfInteger989.java
+++ src/AddToArrayFormOfInteger989.java
@@ -5,8 +5,29 @@
  *   File: AddToArrayFormOfInteger989.java
  */
 
+import java.util.Arrays;
+
 public class AddToArrayFormOfInteger989 {
     public static void main(String[] args) {
+
+        int [] arr = {1,2,3,4};
+        int n = 12;
+        int [] arr1 = new int[arr.length];
+
+        int factor = 0;
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result = arr[i] + factor * 10;
+            factor = result;
+        }
+
+        result = result+n;
+        for(int i=0; i<arr1.length-1; i++){
+            arr1[i] = 
+        }
+
+
+        System.out.println(Arrays.toString(result));
 
     }
 }
