1597a783e8072611989539a2e934aab296799002
Gautam <gautamsethigagan@gmail.com>
2022-03-18 23:58:22 +0530

    .

--- src/AddToArrayFormOfInteger989.java
+++ src/AddToArrayFormOfInteger989.java
@@ -13,22 +13,24 @@ import java.util.List;
 public class AddToArrayFormOfInteger989 {
     public static void main(String[] args) {
 
-        public List<Integer> addToArrayForm(int[] num, int k) {
-            int n = num.length;
-            ArrayList<Integer> list = new ArrayList<>();
-            for(int i = n - 1; i >= 0; i--) {
-                int sum = num[i] + k;
-                list.add(sum % 10); // Inserting at the end of arraylist takes O(1) time.
-                k = sum / 10;
-            }
-            while(k > 0) {
-                list.add(k % 10);
-                k = k / 10;
-            }
-            Collections.reverse(list); // Reversing will take O(n) time.
-            return list;
 
+    }
+
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        int n = num.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = num[i] + k;
+            list.add(sum % 10); // Inserting at the end of arraylist takes O(1) time.
+            k = sum / 10;
         }
+        while (k > 0) {
+            list.add(k % 10);
+            k = k / 10;
+        }
+        Collections.reverse(list); // Reversing will take O(n) time.
+        return list;
 
     }
 }
+
