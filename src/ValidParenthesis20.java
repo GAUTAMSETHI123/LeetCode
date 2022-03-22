e2d791192bfb952bafdca7e81d423e34f1657631
Gautam <gautamsethigagan@gmail.com>
2022-03-22 23:55:02 +0530

    change in function

--- src/ValidParenthesis20.java
+++ src/ValidParenthesis20.java
@@ -8,19 +8,30 @@
 import java.util.Stack;
 
 public class ValidParenthesis20 {
-    Stack<Character> bracket = new Stack<>();
+    public static boolean isValid(String s) {
+        Stack<Character> bracket = new Stack<>();
         for (char c : s.toCharArray()) {
-        switch (c) {
-            case '(': bracket.push(c); break;
-            case '{': bracket.push(c); break;
-            case '[': bracket.push(c); break;
-            case ')': if (bracket.empty() || bracket.peek()!='(') return false; else bracket.pop(); break;
-            case '}': if (bracket.empty() || bracket.peek()!='{') return false; else bracket.pop(); break;
-            case ']': if (bracket.empty() || bracket.peek()!='[') return false; else bracket.pop(); break;
-            default: ;
+            switch (c) {
+                case '(': bracket.push(c); break;
+                case '{': bracket.push(c); break;
+                case '[': bracket.push(c); break;
+                case ')': if (bracket.empty() || bracket.peek()!='(') return false; else bracket.pop(); break;
+                case '}': if (bracket.empty() || bracket.peek()!='{') return false; else bracket.pop(); break;
+                case ']': if (bracket.empty() || bracket.peek()!='[') return false; else bracket.pop(); break;
+                default:
+            }
+        }
+        return bracket.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s = "{}[)(]";
+        boolean check = isValid(s);
+        if(check){
+            System.out.println("The given string contains valid parentheses.");
+        }
+        else{
+            System.out.println("The given string contains invalid parentheses.");
         }
     }
-
-
-        return bracket.isEmpty();
 }
+
