b07a8a7c510f9bcc0d1d73525e67dafb87511835
Gautam <gautamsethigagan@gmail.com>
2022-03-25 23:50:41 +0530

    Fizz Buzz Leetcode 412

---
+++ src/FizzBuzz412.java
@@ -0,0 +1,28 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gautam Sethi (GAUTAMSETHI123)
+ *   Date: 25-03-2022
+ *   Time: 23:50
+ *   File: FizzBuzz412.java
+ */
+
+public class FizzBuzz412 {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> list = new ArrayList<String>();
+        for(int i = 1; i<=n; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                list.add("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                list.add("Fizz");
+            }
+            else if(i % 5 == 0){
+                list.add("Buzz");
+            }
+            else{
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+
+    }
+}
